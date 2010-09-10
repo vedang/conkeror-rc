0693d73de2e4237a166c9f542300981c187a2e65
Vedang Manerikar <ved.manerikar@gmail.com>
2010-09-10 17:08:55 +0530

    Right-click should open links in new buffers and d should follow links in new buffers

--- init.js
+++ init.js
@@ -1,5 +1,7 @@
-// Open Middle-Clicked Links in New Buffers
-require("clicks-in-new-buffer.js");
+// Open Right-Clicked Links in New Buffers
+require ("clicks-in-new-buffer.js");
 clicks_in_new_buffer_target = OPEN_NEW_BUFFER_BACKGROUND;
+clicks_in_new_buffer_button = 2;
 
-
+// follow link in new buffer
+define_key(content_buffer_normal_keymap, "d", "follow-new-buffer");
