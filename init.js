adb9b6b863353d3de2d33479b1791e7661209dae
Vedang Manerikar <ved.manerikar@gmail.com>
2010-09-10 17:51:32 +0530

    added some more minor customizations

--- init.js
+++ init.js
@@ -5,3 +5,16 @@ clicks_in_new_buffer_button = 2;
 
 // follow link in new buffer
 define_key(content_buffer_normal_keymap, "d", "follow-new-buffer");
+
+// adblockplus is good
+require("extensions/adblockplus.js");
+
+// use emacs as external editor.
+editor_shell_command = "emacs";
+
+// view source in your editor.
+view_source_use_external_editor = true;
+
+// let xkcd-mode put the funny alt text into the page.
+xkcd_add_title = true;
+
