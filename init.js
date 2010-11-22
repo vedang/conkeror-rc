65fb595169c757868a6e7f39dcbc70b10402157c
Vedang Manerikar <ved.manerikar@gmail.com>
2010-11-22 17:34:10 +0530

    housekeeping

--- init.js
+++ init.js
@@ -7,16 +7,13 @@ clicks_in_new_buffer_button = 2;
 define_key(content_buffer_normal_keymap, "d", "follow-new-buffer");
 define_key(content_buffer_normal_keymap, "t", "find-url-new-buffer");
 
-// adblockplus is good
-require("extensions/adblockplus.js");
-
 // i need my gmail hotkeys
-require("page-modes/gmail.js");
+require ("page-modes/gmail.js");
 define_key(gmail_keymap, "t", "find-url-new-buffer");
 define_key(gmail_keymap, "return", null, $fallthrough);
 
 // use emacs as external editor.
-editor_shell_command = "emacs";
+editor_shell_command = "emacsclient";
 
 // view source in your editor.
 view_source_use_external_editor = true;
@@ -32,3 +29,5 @@ let (mozrepl_init = get_home_directory()) {
 // let xkcd-mode put the funny alt text into the page.
 xkcd_add_title = true;
 
+// adblockplus is good
+require("extensions/adblockplus.js");
